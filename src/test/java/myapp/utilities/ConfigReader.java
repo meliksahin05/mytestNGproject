cf2c14334cd2fb7aac1bda65929f9c505ba22eb9
meliksahin05 <meliksahin05@gmail.com>
2023-10-19 12:01:48 +0300

    Utilities

--- src/test/java/myapp/utilities/ConfigReader.java
+++ src/test/java/myapp/utilities/ConfigReader.java
@@ -1,30 +1,40 @@
 package myapp.utilities;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Properties;
 
 public class ConfigReader {
 
-    //    this class is used for reading configuration.properties file
-    private static Properties properties;
+    //    this class is used for reading configuration.properties fill
+    private  static Properties properties;
 
-    //    static block is used for prerequsets
-    static {
+
+    static{
         String path = "configuration.properties";
         try {
             FileInputStream fileInputStream = new FileInputStream(path);
             properties = new Properties();
-            properties.load(fileInputStream); //to load, to read, to open that file
+            properties.load(fileInputStream);
             fileInputStream.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    //    this method accepts KEY returns VALUE. Eg :ConfigReader.getProperty(amazon_url); => https://www.amazon.com
-    public static String getProperty(String key){
+
+    public static String getProperty (String key){
         String value = properties.getProperty(key);
         return value;
+
     }
 
 
+
+
 }
+
+
+
+
+
+
