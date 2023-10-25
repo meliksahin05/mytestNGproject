d8b3f148e841fade2adf2961b3d0184ca8f6f879
meliksahin05 <meliksahin05@gmail.com>
2023-10-25 22:58:02 +0300

    MedunnaLoginPage

---
+++ src/test/java/myapp/pages/MedunnaLoginPage.java
@@ -0,0 +1,31 @@
+package myapp.pages;
+
+import myapp.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MedunnaLoginPage {
+
+    public MedunnaLoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//input[@name='username']")
+    public WebElement userName;
+
+    @FindBy(xpath = "//input[@name='password']")
+    public WebElement passwordInput;
+
+    @FindBy(id = "rememberMe")
+    public WebElement rememberMeCheckbox;
+
+    @FindBy (xpath = "//button[@type='submit']")
+    public WebElement signInSubmitButton;
+
+
+
+
+}
--- src/test/java/myapp/tests/practices/MedunnaPatient.java
+++ src/test/java/myapp/tests/practices/MedunnaPatient.java
@@ -1,7 +1,12 @@
 package myapp.tests.practices;
 
+import myapp.pages.MedunnaHomePage;
+import myapp.pages.MedunnaLoginPage;
+import myapp.utilities.ConfigReader;
 import myapp.utilities.Driver;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 public class MedunnaPatient {
 
@@ -9,25 +14,32 @@ public class MedunnaPatient {
     public void medunnaPatient() {
 //        Go to https://medunna.com/
 
-        Driver.getDriver().get("medunna_url");
+        Driver.getDriver().get(ConfigReader.getProperty("medunna_url"));
 
 //        Click on "user icon"
-
-
+        MedunnaHomePage medunnaHomePage = new MedunnaHomePage();
+        medunnaHomePage.userIcon.click();
 
 //        Click on "Sign In" option
-
+        medunnaHomePage.signInOption.click();
 //        Enter username into "Username" input
-
+        MedunnaLoginPage medunnaLoginPage = new MedunnaLoginPage();
+        medunnaLoginPage.userName.sendKeys("mark_twain");
 //        Enter password into "Password" input
+        medunnaLoginPage.passwordInput.sendKeys("Mark.123");
 
 //        Click on "Remember me" check box
+        medunnaLoginPage.rememberMeCheckbox.click();
+        assertTrue(medunnaLoginPage.rememberMeCheckbox.isSelected());
 
 //        Click on "Sign in" submit button
+        medunnaLoginPage.signInSubmitButton.click();
 
 //        Click on "Items&Titles" menu
+         medunnaHomePage.itemsAndTitlesMenu.click();
 
 //        Click on "Patient" option
+        medunnaHomePage.patientOption.click();
 
 //        Click on "Create a new patient" button
 
