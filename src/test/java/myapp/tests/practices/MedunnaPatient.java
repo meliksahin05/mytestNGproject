e4666d806b52a5d1a78ac241b4d0d44407746718
meliksahin05 <meliksahin05@gmail.com>
2023-10-25 22:27:16 +0300

    MedunnaPatient

---
+++ src/test/java/myapp/tests/practices/MedunnaPatient.java
@@ -0,0 +1,48 @@
+package myapp.tests.practices;
+
+import myapp.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class MedunnaPatient {
+
+    @Test
+    public void medunnaPatient() {
+//        Go to https://medunna.com/
+
+        Driver.getDriver().get("medunna_url");
+
+//        Click on "user icon"
+
+
+
+//        Click on "Sign In" option
+
+//        Enter username into "Username" input
+
+//        Enter password into "Password" input
+
+//        Click on "Remember me" check box
+
+//        Click on "Sign in" submit button
+
+//        Click on "Items&Titles" menu
+
+//        Click on "Patient" option
+
+//        Click on "Create a new patient" button
+
+//        Enter firstname into "First Name" input
+
+//        Enter lastname into "Last Name" input
+
+//        Enter email into "Email" input
+
+//        Enter phone number into "Phone" input
+
+//        Click on "Save" button
+
+//        Assert that patient is created
+
+    }
+
+}
